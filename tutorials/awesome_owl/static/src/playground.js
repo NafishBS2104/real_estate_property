790696cb7801f6576788c3350fc8d929a12d5e62
Nafish <nafish.salehin@brainstation-23.com>
2026-06-15 19:06:28 +0600

    [ADD] awesome_owl: implemented exercise 3

--- tutorials/awesome_owl/static/src/playground.js
+++ tutorials/awesome_owl/static/src/playground.js
@@ -2,9 +2,10 @@
 import { Component,useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Counter } from "./components/Counter/counter";
+import {Card} from "./components/Card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-    static components = { Counter };
+    static components = { Counter, Card};
 }
 registry.category("actions").add("awesome_owl.playground",Playground)
