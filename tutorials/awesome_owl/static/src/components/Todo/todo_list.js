4318daf9b30f4db30150da8a9198e59b255e59b7
Nafish <nafish.salehin@brainstation-23.com>
2026-06-16 18:46:10 +0600

    [ADD] awesome_owl: implemented exercise 9-dding a todo

--- tutorials/awesome_owl/static/src/components/Todo/todo_list.js
+++ tutorials/awesome_owl/static/src/components/Todo/todo_list.js
@@ -1,27 +1,47 @@
 /** @odoo-module **/
-import {useState,Component} from "@odoo/owl";
+import {useState,Component,xml} from "@odoo/owl";
 import {TodoItem} from "./todo_item";
 
 
 export class Todo extends Component{
-    static template = "awesome_owl.TodoList";
+    static template = xml`
+        <div class="todo-list">
+            <!-- New input field -->
+            <input
+               type="text"
+               placeholder="Enter a new task"
+               t-on-keyup="addTodo"/>
+
+                <!-- Existing todo list -->
+                <t t-foreach="this.todos" t-as="todo" t-key="todo.id">
+                  <TodoItem todo="todo"/>
+                </t>
+           </div>
+    `;
 
     static components = {
         TodoItem,
     };
 
     setup(){
-        this.todos = useState([
+        this.todos = useState([]);
+        this.nextId = 1;
+    }
+
+    addTodo(ev){
+        if(ev.keyCode !== 13){
+            return;
+        }
+        const input = ev.target;
+        const description = input.value.trim();
+
+        this.todos.push(
             {
-                id: 3,
-                description: "buy milk",
-                isCompleted: false,
-            },
-            {
-              id: 2,
-              description: "Write Tutorials",
-              isCompleted: true,
-            },
-        ]);
+                id: this.nextId++,
+                description: description,
+                isComplete: false,
+            }
+        );
+        this.value = "";
     }
 }
