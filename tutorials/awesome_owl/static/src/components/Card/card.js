47e3926c13da8585b462212c658568f9b061117a
Nafish <nafish.salehin@brainstation-23.com>
2026-06-17 17:38:59 +0600

    [ADD] awesome_owl: implemented exercise 14-minimizing card content

--- tutorials/awesome_owl/static/src/components/Card/card.js
+++ tutorials/awesome_owl/static/src/components/Card/card.js
@@ -1,5 +1,5 @@
 /** @odoo-module **/
-import {Component} from "@odoo/owl";
+import {Component,useState} from "@odoo/owl";
 
 export class Card extends Component{
     static template = "awesome_owl.Card";
@@ -14,4 +14,12 @@ export class Card extends Component{
           },
         },
     };
+    setuo(){
+        this.state = useState({
+            isOpen: true,
+        });
+    }
+    toggleContent(){
+        this.state.isOpen = !this.state.isOpen;
+    }
 }
