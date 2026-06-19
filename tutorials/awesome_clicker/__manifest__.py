043286508d9cd8c277c021f87bf77b486c56952f
Nafish <nafish.salehin@brainstation-23.com>
2026-06-19 15:43:05 +0600

    [ADD] awesome_clicker: implemented exercise 1

---
+++ tutorials/awesome_clicker/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Awesome Clicker",
+
+    'author': "Brain Station 23",
+    'category': 'Tutorials',
+    'version': '1.0.0',
+    'application': True,
+    'installable': True,
+    'depends': ['base', 'web'],
+    'data': [],
+    'assets': {
+        'web.assets_backend': [
+            'awesome_clicker/static/src/**/*',
+        ],
+
+    },
+    'license': 'AGPL-3'
+}
