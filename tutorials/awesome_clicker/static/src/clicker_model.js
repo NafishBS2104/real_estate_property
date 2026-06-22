5ecf096dbc2c8ba58295cbd0270f83d26a84ed28
Nafish <nafish.salehin@brainstation-23.com>
2026-06-22 13:03:31 +0600

    [ADD] awesome_owl: implemented exercise 16-Add yet another resouce: trees

--- tutorials/awesome_clicker/static/src/clicker_model.js
+++ tutorials/awesome_clicker/static/src/clicker_model.js
@@ -24,6 +24,24 @@ export class ClickerModel extends Reactive{
                 purchased: 0,
             }
         }
+        this.trees = {
+            pearTree: {
+                price: 1000000,
+                level: 4,
+                produce: "pear",
+                purchased: 0,
+            },
+            cherryTree: {
+                price: 1000000,
+                level: 4,
+                produce: "cherry",
+                purchased: 0,
+            },
+        }
+        this.fruits = {
+            pear: 0,
+            cherry: 0,
+        },
         this.multiplier = 1
 
         document.addEventListener("click",() => this.increment(1), true);
@@ -32,6 +50,12 @@ export class ClickerModel extends Reactive{
                 this.clicks += this.bots[bot].increment * this.bots[bot].purchased * this.multiplier;
             }
         }, 10000);
+
+        setInterval(() => {
+            for(const tree in this.trees){
+                this.fruits[this.trees[tree].produce] += this.trees[tree].purchased;
+            }
+        },30000);
     }
 
     buyMultiplier(){
@@ -77,6 +101,18 @@ export class ClickerModel extends Reactive{
         const reward = choose(availableReward);
         this.bus.trigger("REWARD", reward);
         return choose(availableReward);
+    }
+
+    buyTree(name){
+        if(!Object.keys(this.trees).includes(name)){
+            throw new Error(`Invalid tree name ${name}`);
+        }
+        if(this.clicks < this.trees[name].price){
+            return false;
+        }
+        this.clicks -= this.trees[name].price;
+        this.trees[name].purchased++;
+
     }
 
     get milestones(){
