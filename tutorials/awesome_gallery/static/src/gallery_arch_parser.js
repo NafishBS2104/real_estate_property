ef132e90aba9dbaa58a5ebc0337f0466c274b830
Nafish <nafish.salehin@brainstation-23.com>
2026-06-23 16:59:28 +0600

    [ADD] awesome_gallery: implemented exercise 3 - Parse the arch

---
+++ tutorials/awesome_gallery/static/src/gallery_arch_parser.js
@@ -0,0 +1,9 @@
+/** @odoo-module */
+export class GalleryArchParser{
+    parse(xmlDoc){
+        const imageField = xmlDoc.getAttribute("image_field");
+        return {
+            imageField,
+        };
+    }
+}
--- tutorials/awesome_gallery/static/src/gallery_controller.js
+++ tutorials/awesome_gallery/static/src/gallery_controller.js
@@ -7,6 +7,7 @@ export class GalleryController extends Component{
     static template = "awesome_gallery.GalleryController";
     static props = {
         ...standardViewProps,
+        archInfo: Object,
     };
     static components = {Layout};
 }
