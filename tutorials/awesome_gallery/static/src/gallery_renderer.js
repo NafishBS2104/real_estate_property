a4c7cdd9128e0d6542e0c7a24d18a34f8b8ab504
Nafish <nafish.salehin@brainstation-23.com>
2026-06-25 19:19:58 +0600

    [ADD] awesome_gallery: implemented exercise 8 - Display Images

--- tutorials/awesome_gallery/static/src/gallery_renderer.js
+++ tutorials/awesome_gallery/static/src/gallery_renderer.js
@@ -1,8 +1,10 @@
 /** @odoo-module **/
 import {Component} from "@odoo/owl";
 import {GalleryModel} from "./gallery_model";
+import {GalleryImage} from "./gallery_image";
 
 export class GalleryRenderer extends Component{
+    static components = {GalleryImage};
     static template = "awesome_gallery.GalleryRenderer";
     static props = {
         model: GalleryModel,
