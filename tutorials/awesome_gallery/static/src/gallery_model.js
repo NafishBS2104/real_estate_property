8af785e46d62c75d5dbb5e3165a17532b97a9eb9
Nafish <nafish.salehin@brainstation-23.com>
2026-06-25 20:04:47 +0600

    [ADD] awesome_gallery: implemented exercise 10-Add an optional tooltip

--- tutorials/awesome_gallery/static/src/gallery_model.js
+++ tutorials/awesome_gallery/static/src/gallery_model.js
@@ -2,10 +2,12 @@
 import {KeepLast} from "@web/core/utils/concurrency";
 
 export class GalleryModel{
-    constructor(orm,resModel,archInfo){
+    constructor(orm,resModel,fields,archInfo){
         this.orm = orm;
         this.resModel = resModel;
-        const {imageField,limit} = archInfo;
+        const {imageField,limit,tooltipField} = archInfo;
+        this.fields = fields;
+        this.tooltipField = tooltipField;
         this.imageField = imageField;
         this.limit = limit;
         this.keepLast = new KeepLast();
@@ -17,12 +19,33 @@ export class GalleryModel{
                 limit: this.limit,
                 specification: {
                     [this.imageField]: {},
+                    ...(this.tooltipField ? {[this.tooltipField]: {}}: {}),
                 },
                 context: {
                     bin_size: true,
                 }
             })
         );
-        this.records = records;
+        if(!this.tooltipField) {
+            this.records = records;
+            return;
+        }
+
+        switch (this.fields[this.tooltipField].type) {
+            case "many2one":
+                this.records = records.map((record) => ({
+                    ...record,
+                    [this.tooltipField]: record[this.tooltipField][1],
+                }));
+                break;
+            case "integer":
+                this.records = records.map((record) => ({
+                    ...record,
+                    [this.tooltipField]: String(record[this.tooltipField]),
+                }));
+                break;
+            default:
+                this.records = records;
+        }
     }
 }
