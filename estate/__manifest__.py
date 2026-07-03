9acb6d984c2a3a2f8c628c93e64f787c4ef603a7
Nafish <nafish.salehin@brainstation-23.com>
2026-07-03 14:44:21 +0600

    Initial Commmit

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+   'name' : "Estate ",
+   'version' : "1.0",
+   'category' : "Real Estate",
+   'description' : """Real Estate management""",
+   'author' : "Brain Station 23",
+   'depends' : ['base'],
+   'installable' : True,
+   'application' : True,
+}
