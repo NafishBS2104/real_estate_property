cadbb67647f843070894db174dd02990843d7fbd
NafishBS2104 <nafish.salehin@brainstation-23.com>
2026-07-05 17:38:40 +0600

    Delete estate/models/estate_property_type.py

--- estate/models/estate_property_type.py
+++
@@ -1,6 +0,0 @@
-from odoo import models,fields
-
-class EstatePropertyType(models.Model):
-    _name = "estate.property.type"
-    _description = "Estate property type"
-    name=fields.Char(required=True,string="Name")
