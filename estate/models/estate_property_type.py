c24d8481314af90684b6c3e7d5a6da25687b9daf
Nafish <nafish.salehin@brainstation-23.com>
2026-07-05 16:34:01 +0600

    [ADD] estate: implemented chapter 6,7

---
+++ estate/models/estate_property_type.py
@@ -0,0 +1,6 @@
+from odoo import models,fields
+
+class EstatePropertyType(models.Model):
+    _name = "estate.property.type"
+    _description = "Estate property type"
+    name=fields.Char(required=True,string="Name")
