ff8dd4b3d378a50f2d2cff8b00732677cd022ca1
Nafish <nafish.salehin@brainstation-23.com>
2026-07-07 18:59:04 +0600

    [ADD] estate: completed chapter 10,11

---
+++ estate/models/estate_property_tag.py
@@ -0,0 +1,17 @@
+from odoo import models, fields
+
+
+class EstatePropertyTag(models.Model):
+    _name='estate.property.tag'
+    _description='Real Estate Property Tag'
+    _order="name"
+
+    name=fields.Char(required=True, string="Name")
+    color=fields.Integer()
+    _sql_constraints = [
+        (
+            'check_name_unique',
+            'unique(name)',
+            'Property Tag Name Must Be Unique'
+        )
+    ]
